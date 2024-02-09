c649aa2592a83bcc46a5ecfb3a29a8542ceba482
Hasan Ali <hasanali.abps@gmail.com>
2024-02-09 23:34:12 +0530

    Update health.ts

--- api/src/routes/health.ts
+++ api/src/routes/health.ts
@@ -3,7 +3,7 @@ import express, {Request, Response} from 'express';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('server is running fine!');
+  res.send('server is running fine.');
 });
 
 export default router;
